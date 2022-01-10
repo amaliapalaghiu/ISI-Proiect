c4e84bfc5aee725a5aaf63a319ca966b2cea817e
Palaghiu Amalia <amaliapalaghiu@yahoo.com>
2022-01-10 16:12:03 +0200

    Add choose user-type

--- app/signup/signup.js
+++ app/signup/signup.js
@@ -11,22 +11,42 @@ angular.module('myApp.signup', ['ngRoute'])
 
 .controller('SignupCtrl', ['$scope', '$location', '$http', function($scope, $location, $http) {
   $scope.signupButtonClick = function() {
-    $http({
-      method: 'POST',
-      url: 'http://localhost:3000/api/users/add',
-      data: { 
-        username: $scope.username,
-        passwrd: $scope.password,
-        firstName: $scope.firstName, 
-        lastName: $scope.lastName,
-        telephone: $scope.telephone,
-        email: $scope.email,
-        user_type: 1
-      }
-    }).then(function successCallback(response) {
-        $location.path('/login');
-      }, function errorCallback(response) {
-        
-      });
+    if ($scope.userType === "Transportator") {
+      $http({
+        method: 'POST',
+        url: 'http://localhost:3000/api/users/add',
+        data: { 
+          username: $scope.username,
+          passwrd: $scope.password,
+          firstName: $scope.firstName, 
+          lastName: $scope.lastName,
+          telephone: $scope.telephone,
+          email: $scope.email,
+          user_type: 1
+        }
+      }).then(function successCallback(response) {
+          $location.path('/login');
+        }, function errorCallback(response) {
+          
+        });
+    } else if ($scope.userType === "Expeditor") {
+      $http({
+        method: 'POST',
+        url: 'http://localhost:3000/api/users/add',
+        data: { 
+          username: $scope.username,
+          passwrd: $scope.password,
+          firstName: $scope.firstName, 
+          lastName: $scope.lastName,
+          telephone: $scope.telephone,
+          email: $scope.email,
+          user_type: 2
+        }
+      }).then(function successCallback(response) {
+          $location.path('/login');
+        }, function errorCallback(response) {
+          
+        });
+    }
   }
 }]);
