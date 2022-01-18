c8d141f57a4581a0c01e40a5c8b190227390b99d
Liviu Drăgulin <liviu.dragulin@stud.acs.upb.ro>
2022-01-18 13:56:41 +0200

    add redirect to map

--- app/dashboardtrans/dashboardtrans.js
+++ app/dashboardtrans/dashboardtrans.js
@@ -2,48 +2,48 @@
 
 angular.module('myApp.dashboardtrans', ['ngRoute'])
 
-  .config(['$routeProvider', function ($routeProvider) {
-    $routeProvider.when('/dashboardtrans', {
-      templateUrl: 'dashboardtrans/dashboardtrans.html',
-      controller: 'DashboardTransCtrl'
-    });
-  }])
-  .controller('DashboardTransCtrl', ['$scope', '$location', function($scope, $location) {
-    $scope.hartaTransClick = function() {
-      $location.path('/dashboardtrans/harta');
-    }
-    $scope.cereriClientiTransClick = function() {
-      $location.path('/dashboardtrans/listacereri');
-    }
-    $scope.oferteTransClick = function() {
-      $location.path('/dashboardtrans/oferte');
-    }
-    $scope.camioaneTransClick = function() {
-      $location.path('/dashboardtrans/camioane');
-    }
+	.config(['$routeProvider', function ($routeProvider) {
+		$routeProvider.when('/dashboardtrans', {
+			templateUrl: 'dashboardtrans/dashboardtrans.html',
+			controller: 'DashboardTransCtrl'
+		});
+	}])
+	.controller('DashboardTransCtrl', ['$scope', '$location', function ($scope, $location) {
+		$scope.hartaTransClick = function () {
+			$location.path('/dashboardtrans/harta');
+		}
+		$scope.cereriClientiTransClick = function () {
+			$location.path('/dashboardtrans/listacereri');
+		}
+		$scope.oferteTransClick = function () {
+			$location.path('/dashboardtrans/oferte');
+		}
+		$scope.camioaneTransClick = function () {
+			$location.path('/dashboardtrans/camioane');
+		}
 
-    $scope.$on('$viewContentLoaded', function() {
-      var slideIndex = 0;
-      showSlides();
+		$scope.$on('$viewContentLoaded', function () {
+			var slideIndex = 0;
+			showSlides();
 
-      function showSlides() {
-        var i;
-        var slides = document.getElementsByClassName("mySlides");
-        var dots = document.getElementsByClassName("dot");
-        for (i = 0; i < slides.length; i++) {
-          slides[i].style.display = "none";  
-        }
-        slideIndex++;
-        if (slideIndex > slides.length) {slideIndex = 1}    
-        for (i = 0; i < dots.length; i++) {
-          dots[i].className = dots[i].className.replace(" active", "");
-        }
-        slides[slideIndex-1].style.display = "block";  
-        dots[slideIndex-1].className += " active";
-        if (slideIndex == 4) {
-          slideIndex = 0;
-        }
-        setTimeout(showSlides, 2000);
-      }
-    })
-  }]);
+			function showSlides() {
+				var i;
+				var slides = document.getElementsByClassName("mySlides");
+				var dots = document.getElementsByClassName("dot");
+				for (i = 0; i < slides.length; i++) {
+					slides[i].style.display = "none";
+				}
+				slideIndex++;
+				if (slideIndex > slides.length) { slideIndex = 1 }
+				for (i = 0; i < dots.length; i++) {
+					dots[i].className = dots[i].className.replace(" active", "");
+				}
+				slides[slideIndex - 1].style.display = "block";
+				dots[slideIndex - 1].className += " active";
+				if (slideIndex == 4) {
+					slideIndex = 0;
+				}
+				setTimeout(showSlides, 2000);
+			}
+		})
+	}]);
