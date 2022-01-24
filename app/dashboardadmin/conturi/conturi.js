4705a63a3b36e2d4a309a48665fc7e506a5f2be3
aureliacadar <cadar_aurelia@yahoo.com>
2022-01-24 18:45:20 +0200

    Added add new user modal

--- app/dashboardadmin/conturi/conturi.js
+++ app/dashboardadmin/conturi/conturi.js
@@ -20,5 +20,71 @@ angular.module('myApp.conturi', ['ngRoute'])
       $location.path('/dashboardadmin');
     }
 
+    $scope.openModalUser  = function(){
+        $uibModal.open({
+          templateUrl : '/dashboardadmin/conturi/addUserModal.html',
+          backdrop: true
+        });
+      };
+
+      $scope.submitCont = function () {
+        //console.log($scope.cont);
+        if ($scope.cont.userType === "Transportator") {
+            $http({
+              method: 'POST',
+              url: 'http://localhost:3000/api/users/add',
+              data: { 
+                username: $scope.cont.username,
+                passwrd: $scope.cont.password,
+                firstName: $scope.cont.firstName, 
+                lastName: $scope.cont.lastName,
+                telephone: $scope.cont.telephone,
+                email: $scope.cont.email,
+                user_type: 1
+              }
+            }).then(function successCallback(response) {
+                window.location.reload();
+              }, function errorCallback(response) {
+                
+              });
+          } else if ($scope.cont.userType === "Expeditor") {
+            $http({
+              method: 'POST',
+              url: 'http://localhost:3000/api/users/add',
+              data: { 
+                username: $scope.cont.username,
+                passwrd: $scope.cont.password,
+                firstName: $scope.cont.firstName, 
+                lastName: $scope.cont.lastName,
+                telephone: $scope.cont.telephone,
+                email: $scope.cont.email,
+                user_type: 2
+              }
+            }).then(function successCallback(response) {
+                window.location.reload();
+              }, function errorCallback(response) {
+                
+              });
+          } else if ($scope.cont.userType === "Admin") {
+            $http({
+              method: 'POST',
+              url: 'http://localhost:3000/api/users/add',
+              data: { 
+                username: $scope.cont.username,
+                passwrd: $scope.cont.password,
+                firstName: $scope.cont.firstName, 
+                lastName: $scope.cont.lastName,
+                telephone: $scope.cont.telephone,
+                email: $scope.cont.email,
+                user_type: 3
+              }
+            }).then(function successCallback(response) {
+                window.location.reload();
+              }, function errorCallback(response) {
+                
+              });
+          }
+      }
+
 }
 ]);
